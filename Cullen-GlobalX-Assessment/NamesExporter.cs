4fb85abaff0398773a89715c32124484bab94330
James Cullen <jimcullenaus@gmail.com>
2018-02-08 01:12:50 +1100

    Add file and console exporter

---
+++ Cullen-GlobalX-Assessment/NamesExporter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace CullenGlobalXAssessment {
+    public class NamesExporter {
+        public static void exportNames(List<Person> people) {
+            try {
+                using (System.IO.StreamWriter file = new StreamWriter("sorted-names-list.txt")) {
+                    foreach (Person person in people) {
+                        file.WriteLine(person);
+                        Console.WriteLine(person);
+                    }
+                }
+            } catch (IOException e) {
+                Console.Error.WriteLine("Was unable to write to the file.");
+            }
+        }
+    }
+}
