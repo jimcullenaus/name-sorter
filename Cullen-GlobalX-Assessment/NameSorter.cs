98ca45fc80492bbe33c3bcc9fcce0c5995996026
James Cullen <jimcullenaus@gmail.com>
2018-02-07 18:56:02 +1100

    Initial commit

---
+++ Cullen-GlobalX-Assessment/NameSorter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cullen_GlobalX_Assessment
+{
+    class NameSorter
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
