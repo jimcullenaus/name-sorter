319eb2f07de401500bd026e055b68d5e64234904
James Cullen <jimcullenaus@gmail.com>
2018-02-08 03:37:16 +1100

    Test sorting

--- CullenGlobalXAssessmentTests/ImportTester.cs
+++ CullenGlobalXAssessmentTests/ImportTester.cs
@@ -2,8 +2,6 @@ using System;
 using Xunit;
 using CullenGlobalXAssessment;
 using System.Collections.Generic;
-using System.IO;
-using System.Reflection;
 
 namespace CullenGlobalXAssessmentTests {
     public class ImportTester {
@@ -39,28 +37,25 @@ namespace CullenGlobalXAssessmentTests {
             for (int i = 0; i < firstNames.Count; ++i) {
                 people.Add(new Person(firstNames[i], lastNames[i]));
             }
-            var testNames = NamesImporter.importNames("/home/travis/build/jimcullenaus/name-sorter/CullenGlobalXAssessmentTests/test-samples/simple-names.txt");
+            var testNames = NamesImporter.importNames("../../../test-samples/simple-names.txt");
             Assert.Equal(testNames, people);
         }
 
         [Fact]
         public void emptyFileTest() {
             var testName = new List<Person> { };
-            var people = NamesImporter.importNames("/home/travis/build/jimcullenaus/name-sorter/CullenGlobalXAssessmentTests/test-samples/blank.txt");
+            var people = NamesImporter.importNames("../../../test-samples/blank.txt");
             Assert.Equal(testName, people);
         }
 
         [Fact]
         public void invalidNameTest() {
-            Assert.Throws<InvalidNamesFileException>(() => NamesImporter.importNames("/home/travis/build/jimcullenaus/name-sorter/CullenGlobalXAssessmentTests/test-samples/no-first.txt"));
+            Assert.Throws<InvalidNamesFileException>(() => NamesImporter.importNames("../../../test-samples/no-first.txt"));
         }
 
         [Fact]
         public void noPermissionTest() {
-            var codeBaseUrl = new Uri(Assembly.GetExecutingAssembly().CodeBase);
-            var codeBasePath = Uri.UnescapeDataString(codeBaseUrl.AbsolutePath);
-            var dirPath = Path.GetDirectoryName(codeBasePath);
-            Assert.Throws<InvalidNamesFileException>(() => NamesImporter.importNames("/home/travis/build/jimcullenaus/name-sorter/CullenGlobalXAssessmentTests/test-samples/no-permission.txt"));
+            Assert.Throws<InvalidNamesFileException>(() => NamesImporter.importNames("../../../test-samples/no-permission.txt"));
         }
     }
 }
---
+++ CullenGlobalXAssessmentTests/SortTester.cs
@@ -0,0 +1,169 @@
+﻿using Xunit;
+using CullenGlobalXAssessment;
+using System.Collections.Generic;
+
+namespace CullenGlobalXAssessmentTests {
+    public class SortTester {
+        [Fact]
+        public void simpleSortTest() {
+            var firstNames = new List<string> {
+                "Janet",
+                "Vaughn",
+                "Adonis Julius",
+                "Shelby Nathan",
+                "Marin",
+                "London",
+                "Beau Tristan",
+                "Leo",
+                "Hunter Uriah Mathew",
+                "Mikayla",
+                "Frankie Conner"
+            };
+            var lastNames = new List<string> {
+                "Parsons",
+                "Lewis",
+                "Archer",
+                "Yoder",
+                "Alvarez",
+                "Lindsey",
+                "Bentley",
+                "Gardner",
+                "Clarke",
+                "Lopez",
+                "Ritter"
+            };
+            var unsorted = personListMaker(firstNames, lastNames);
+            var testSorted = Sorter.sort(unsorted);
+
+            var sortedFirstNames = new List<string> {
+                "Marin",
+                "Adonis Julius",
+                "Beau Tristan",
+                "Hunter Uriah Mathew",
+                "Leo",
+                "Vaughn",
+                "London",
+                "Mikayla",
+                "Janet",
+                "Frankie Conner",
+                "Shelby Nathan"
+            };
+            var sortedLastNames = new List<string> {
+                "Alvarez",
+                "Archer",
+                "Bentley",
+                "Clarke",
+                "Gardner",
+                "Lewis",
+                "Lindsey",
+                "Lopez",
+                "Parsons",
+                "Ritter",
+                "Yoder"
+            };
+            var correctSorted = personListMaker(sortedFirstNames, sortedLastNames);
+            Assert.Equal(correctSorted, testSorted);
+        }
+
+        [Fact]
+        public void sameLastTester() {
+            var unsortedFirst = new List<string> {
+                "Lisa",
+                "Tony",
+                "Keun Young",
+                "John",
+                "Michael",
+                "Mai",
+                "Michelle"
+            };
+            var unsortedLast = new List<string> {
+                "Smith",
+                "Nguyen",
+                "Kim",
+                "Smith",
+                "Kim",
+                "Nguyen",
+                "Nguyen"
+            };
+            var sortedFirst = new List<string> {
+                "Keun Young",
+                "Michael",
+                "Mai",
+                "Michelle",
+                "Tony",
+                "John",
+                "Lisa"
+            };
+            var sortedLast = new List<string> {
+                "Kim",
+                "Kim",
+                "Nguyen",
+                "Nguyen",
+                "Nguyen",
+                "Smith",
+                "Smith"
+            };
+            var unsorted = personListMaker(unsortedFirst, unsortedLast);
+            var testSorted = Sorter.sort(unsorted);
+            var correctSorted = personListMaker(sortedFirst, sortedLast);
+            Assert.Equal(correctSorted, testSorted);
+        }
+
+        [Fact]
+        public void sameFirstTester() {
+            var unsortedFirst = new List<string> {
+                "Richard",
+                "Johann Senior",
+                "Ludwig van",
+                "Johann Sebastian",
+                "Gustav",
+                "Johann Junior",
+                "Richard",
+                "Gustav"
+            };
+            var unsortedLast = new List<string> {
+                "Strauss",
+                "Strauss",
+                "Beethoven",
+                "Bach",
+                "Holst",
+                "Strauss",
+                "Wagner",
+                "Mahler"
+            };
+            var sortedFirst = new List<string> {
+                "Johann Sebastian",
+                "Ludwig van",
+                "Gustav",
+                "Gustav",
+                "Johann Junior",
+                "Johann Senior",
+                "Richard",
+                "Richard"
+            };
+            var sortedLast = new List<string> {
+                "Bach",
+                "Beethoven",
+                "Holst",
+                "Mahler",
+                "Strauss",
+                "Strauss",
+                "Strauss",
+                "Wagner"
+            };
+            var unsorted = personListMaker(unsortedFirst, unsortedLast);
+            var testSorted = Sorter.sort(unsorted);
+            var correctSorted = personListMaker(sortedFirst, sortedLast);
+            Assert.Equal(correctSorted, testSorted);
+        }
+
+        private List<Person> personListMaker(List<string> firstNames, List<string> lastNames) {
+            var people = new List<Person> { };
+            for (int i = 0; i < firstNames.Count; ++i)
+            {
+                people.Add(new Person(firstNames[i], lastNames[i]));
+            }
+            return people;
+        }
+    }
+}
